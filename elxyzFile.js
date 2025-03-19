2673775600cd75a869ebd4ed03a7f279f2c82f0c
goodboys21 <akunceeingood5@gmail.com>
2025-03-20 02:58:18 +0700

    elxyzFile.js

--- elxyzFile.js
+++ elxyzFile.js
@@ -1,29 +1,32 @@
 const axios = require("axios");
 const fs = require("fs");
 const FormData = require("form-data");
+const https = require("https"); // Tambahin ini
 
-const elxyzFile = async (Path) => 
-  new Promise(async (resolve, reject) => {
-    if (!fs.existsSync(Path)) return reject(new Error("File not Found"));
+async function elxyzFile(Path) {
+    return new Promise(async (resolve, reject) => {
+        if (!fs.existsSync(Path)) return reject(new Error("File not Found"));
 
-    try {
-      const form = new FormData();
-      form.append("file", fs.createReadStream(Path));
+        try {
+            const form = new FormData();
+            form.append("file", fs.createReadStream(Path));
 
-      const response = await axios.post('https://cdn.bgs.ct.ws/upload', form, {
-        headers: form.getHeaders(),
-        onUploadProgress: (progressEvent) => {
-          if (progressEvent.lengthComputable) {
-            console.log(`🚀 Upload Progress: ${(progressEvent.loaded * 100) / progressEvent.total}%`);
-          }
+            const response = await axios.post("https://cdn.bgs.ct.ws/index.php", form, {
+                headers: form.getHeaders(),
+                httpsAgent: new https.Agent({ rejectUnauthorized: false }), // Fix SSL
+                onUploadProgress: (progressEvent) => {
+                    if (progressEvent.lengthComputable) {
+                        console.log(`Upload Progress: ${(progressEvent.loaded * 100) / progressEvent.total}%`);
+                    }
+                }
+            });
+
+            resolve(response.data);
+        } catch (error) {
+            console.error("Upload Failed:", error);
+            reject(error);
         }
-      });
-
-      resolve(response.data);
-    } catch (error) {
-      console.error('Upload Failed:', error);
-      reject(error);
-    }
-  });
+    });
+}
 
 module.exports = elxyzFile;
